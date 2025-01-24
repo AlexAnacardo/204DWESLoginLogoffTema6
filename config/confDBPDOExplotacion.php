753be8802bc3a08120605c3e3e3024d77ed41a37
Alex <alex.asesan@educa.jcyl.es>
2025-01-24 13:17:08 +0100

    Modificados scripts de base de datos

--- config/confDBPDOExplotacion.php
+++ config/confDBPDOExplotacion.php
@@ -2,7 +2,7 @@
 
     
     //Ip de la maquina donde esta alojada, puerto que se usara y nombre de la base de datos a conectarse
-    define('CONEXION', 'mysql:host=localhost;port=3306;dbname=DB204DWESLoginLogoffTema6');
+    define('CONEXION', 'mysql:host=localhost;port=3306;dbname=DB204DWES');
     
     //Nombre del usuario que usaremos para conectarnos
     define('USUARIO', 'user204DWESLoginLogoffTema6');
